857fd27cb3349230bcc4ec0a8f975d535a5b1c18
Saurabh-Patil-98 <sp413523@gmail.com>
2023-03-11 12:44:58 +0530

    LL

---
+++ LinkedLIst/src/com/saurabh/LL/Ll1.java
@@ -0,0 +1,16 @@
+package com.saurabh.LL;
+
+import java.util.LinkedList;
+
+public class Ll1 {
+
+	public static void main(String[] args) {
+		LinkedList l1 = new LinkedList();
+		l1.add(100);
+		l1.add('A');
+		l1.add(4.98);
+		l1.add("Saurabh");
+		l1.add(null);
+		System.out.println(l1);
+	}
+}
